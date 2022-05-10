1ece3cade1523334cd6aee64300bf7d0e01b445d
Milena Sviatek <porstudag@gmail.com>
2022-05-10 11:56:11 -0300

    ainda com alguns erros

--- projeto.py
+++ projeto.py
@@ -2,13 +2,35 @@ from random import randint
 
 
 print("Bem-vinde ao JOGO da TABUADA! \nFunciona assim: Escolha a dificuldade e resolva as contas:")
-dificuldade = input('Escolha a dificuldade: \n Fácil[1] \nModerado[2] \Hardmode[3]')
+dificuldade = input('Escolha a dificuldade: \nFácil[1] \nModerado[2] \nHardmode[3]')
 contador = 0
+contador2 = 0
 
 if dificuldade == '1':
-    while contador < 5:
+
+    while contador < 3:
         contador = contador + 1
         num1 = randint(0,6)
         num2 = randint(0,6)
+        resposta = input(f'{num1} x {num2} = ')
         resolva = num1 * num2
-        resposta = input(f'{num1} x {num2}')
+        if resposta == resolva:
+            contador2 = contador2 + 1
+    print(f'Você acertou {contador2} vezes!')
+        
+
+elif dificuldade == '2':
+    while contador < 7:
+        contador = contador + 1
+        num1 = randint(6,8)
+        num2 = randint(6,8)
+        resolva = num1 * num2
+        resposta = input(f'{num1} x {num2} = ')
+elif dificuldade == '3':
+    while contador > 12:
+        contador = contador + 1
+        num1 = randint(8,12)
+        num2 = randint(8,12)
+        resolva = num1 * num2
+        resposta = input(f'{num1} x {num2} = ')
+
