489c39bc2f2c0e367ad83f0ec860745d93a27bb4
Milena Sviatek <porstudag@gmail.com>
2022-05-23 12:01:42 -0300

    colocando loop

--- Readme.md
+++ Readme.md
@@ -1,3 +1,8 @@
 Olá, mundo. Esse é um projeto de um jogo para resolver contas de multiplicação. 
 Escolha uma dificuldade e divirta-se praticando tabuada!
 (o projeto está em andamento e sendo comitado pelo git)
+
+Bibliotecas:
+Random
+Emoji
+Pygame
--- projeto.py
+++ projeto.py
@@ -1,103 +1,127 @@
+from codecs import replace_errors
+from logging import captureWarnings
 from random import randint
+from time import sleep
 import pygame
+import emoji
 pygame.init()
 
 #Efeitos
-ganhei = pygame.mixer.Sound('coin.mp3.wav')
-errei = pygame.mixer.Sound('errobuzz.wav')
-acerteitudo = pygame.mixer.Sound('winningmusic.wav')
-erreitudo = pygame.mixer.Sound('gameover.wav')
+ganhei = pygame.mixer.Sound('ProjetodaTabuada\musica\coin.mp3.wav')
+errei = pygame.mixer.Sound('ProjetodaTabuada\musica\errobuzz.wav')
+acerteitudo = pygame.mixer.Sound('ProjetodaTabuada\musica\winningmusic.wav')
+erreitudo = pygame.mixer.Sound('ProjetodaTabuada\musica\gameover.wav')
+interface = pygame.mixer.Sound('ProjetodaTabuada\musica\interface.wav')
 
-print("Bem-vinde ao JOGO da TABUADA! \nFunciona assim: Escolha a dificuldade e resolva as contas:")
-dificuldade = input('Escolha a dificuldade: \n[1]Fácil \n[2]Moderado \n[3]Hardmode')
+#cabecalho
+print('=--'* 12)
+print(emoji.emojize(':multiply:  Bem-vinde ao JOGO da TABUADA!:multiply:'))
+print('=--'* 12)
+print('Funciona assim: Escolha a dificuldade e resolva as contas:')
 
 #Contadores
-contador = 0
+contador1 = 0
 contador2 = 0
+contador3 = 0
 
-#FACIL
-if dificuldade == '1':
-    print('Você escolheu FÁCIL: \n')
-    while contador < 5:
-        contador = contador + 1
-        num1 = randint(0,6)
-        num2 = randint(0,6)
-        resposta = input(f'{num1} x {num2} = ')
-        resolva = num1 * num2
+#JOGO
+
+while True:
+    interface.play()
+    dificuldade = input('Escolha a dificuldade: \n\033[0;36m[1]Fácil\033[m \n\033[0;34m[2]Moderado\033[m \n\033[0;35m[3]Hardmode\033[m ')
+    #FACIL
+    if dificuldade == '1':
+        print('Você escolheu FÁCIL: \n')
+        for _ in range(5):
+            num1 = randint(0,6)
+            num2 = randint(0,6)
+            resposta = input(f'{num1} x {num2} = ')
+            resolva = num1 * num2
+                
+            if int(resposta) == resolva:
+                contador1 += 1
+                ganhei.play()
+            if int(resposta) != resolva:
+                errei.play()
+                print(f'Péen! Na verdade é {resolva}')
+
+        print(f'Você acertou {contador1} vezes!')
+        if int(contador1) == 5:
+            acerteitudo.play()
+            print(emoji.emojize(':1st_place_medal: Parabéns, você acertou TODAS!!:partying_face: :party_popper:'))
+            sleep(2)
+        elif int(contador1) < 5 and int(contador2) > 3:
+            print(emoji.emojize(':2nd_place_medal: Você acertou quase tudo! Continue praticando!!'))
+        elif int(contador1) == 0:
+            erreitudo.play()
+            print(emoji.emojize('Uau, você conseguiu errar tudo! Tô impressionade--:frowning_face:'))
+            sleep(2)
+        else:
+            print(emoji.emojize(':3rd_place_medal: Dá pra ver que multiplicação não é seu forte, mas não desista!'))
+
             
-        if int(resposta) == resolva:
-            contador2 = contador2 + 1
+    #MODERADO
+    elif dificuldade == '2':
+        print('Você escolheu MODERADO!')
+        for _ in range(7):
+            num1 = randint(4,8)
+            num2 = randint(4,8)
+            resolva = num1 * num2
+            resposta = input(f'{num1} x {num2} = ')
+            if int(resposta) == resolva:
+                ganhei.play()
+                contador2 += 1
+            if int(resposta) != resolva:
+                errei.play()
+                print(f'Péen! Na verdade é {resolva}')
+        print(f'Você acertou {contador2} vezes!')
+        if int(contador2) == 7:
+            acerteitudo.play()
+            print(emoji.emojize(':1st_place_medal: Parabéns, você acertou TODAS!!:partying_face: :party_popper:'))
+            sleep(2)
+        elif int(contador2) < 7 and int(contador2) > 4:
+            print(emoji.emojize(':2nd_place_medal: Você acertou quase tudo! Continue praticando!!'))
+        elif int(contador2) == 0:
             ganhei.play()
-        if int(resposta) != resolva:
-            errei.play()
-            print(f'Péen! Na verdade é {resolva}')
-
-    print(f'Você acertou {contador2} vezes!')
-    if int(contador2) == 5:
-        acerteitudo.play()
-        print('Parabéns, você acertou TODAS!!')
-    elif int(contador2) < 5 and int(contador2) > 3:
-        print('Você acertou quase tudo! Continue praticando!!')
-    elif int(contador2) == 0:
-        erreitudo.play()
-        print('Uau, você conseguiu errar tudo! Tô impressionade--')
-    else:
-        print('Dá pra ver que multiplicação não é seu forte, mas não desista!')
-
-        
-#MODERADO
-elif dificuldade == '2':
-    print('Você escolheu MODERADO!')
-    while contador < 7:
-        contador = contador + 1
-        num1 = randint(4,8)
-        num2 = randint(4,8)
-        resolva = num1 * num2
-        resposta = input(f'{num1} x {num2} = ')
-        if int(resposta) == resolva:
-            ganhei.play()
-            contador2 = contador2 + 1
-        if int(resposta) != resolva:
-            errei.play()
-            print(f'Péen! Na verdade é {resolva}')
-    print(f'Você acertou {contador2} vezes!')
-    if int(contador2) == 7:
-        acerteitudo.play()
-        print('Parabéns, você acertou TODAS!!')
-    elif int(contador2) < 7 and int(contador2) > 4:
-        print('Você acertou quase tudo! Continue praticando!!')
-    elif int(contador2) == 0:
-        erreitudo.play()
-        print('Uau, você conseguiu errar tudo! Tô impressionade--')
-    else:
-        print('Dá pra ver que multiplicação não é seu forte, mas não desista!')
+            print(emoji.emojize('Uau, você conseguiu errar tudo! Tô impressionade--:frowning_face:'))
+            sleep(2)
+        else:
+            print(emoji.emojize(':3rd_place_medal: Dá pra ver que multiplicação não é seu forte, mas não desista!'))
 
 
-#DIFICIL
-elif dificuldade == '3':
-    print('Você escolheu HARDMODE! \n--Apenas os corajoses escolhem esse nível--')
-    while contador < 10:
-        contador = contador + 1
-        num1 = randint(6,12)
-        num2 = randint(6,12)
-        resolva = num1 * num2
-        resposta = input(f'{num1} x {num2} = ')
-        if int(resposta) == resolva:
-            ganhei.play()
-            contador2 = contador2 + 1
-        if int(resposta) != resolva:
-            errei.play()
-            print(f'Péen! Na verdade é {resolva}')
-    print(f'Você acertou {contador2} vezes!')
-    if int(contador2) == 10:
-        acerteitudo.play()
-        print('Parabéns, você acertou TODAS!!')
-    elif int(contador2) < 10 and int(contador2) > 5:
-        print('Você acertou quase tudo! Continue praticando!!')
-    elif int(contador2) == 0:
-        erreitudo.play()
-        print('Uau, você conseguiu errar tudo! Tô impressionade--')
-    else:
-        print('Dá pra ver que multiplicação não é seu forte, mas não desista!')
-
-
+    #DIFICIL
+    elif dificuldade == '3':
+        print('Você escolheu HARDMODE! \n--Apenas os corajoses escolhem esse nível--')
+        for _ in range(10):
+            num1 = randint(6,12)
+            num2 = randint(6,12)
+            resolva = num1 * num2
+            resposta = input(f'{num1} x {num2} = ')
+            if int(resposta) == resolva:
+                ganhei.play()
+                contador3 += 1
+            if int(resposta) != resolva:
+                errei.play()
+                print(f'Péen! Na verdade é {resolva}')
+        print(f'Você acertou {contador2} vezes!')
+        if int(contador3) == 10:
+            acerteitudo.play()
+            print(emoji.emojize(':1st_place_medal: Parabéns, você acertou TODAS!!:partying_face: :party_popper:'))
+            sleep(2)
+        elif int(contador3) < 10 and int(contador2) > 5:
+            print(emoji.emojize(':2nd_place_medal: Você acertou quase tudo! Continue praticando!!'))
+        elif int(contador3) == 0:
+            erreitudo.play()
+            print(emoji.emojize('Uau, você conseguiu errar tudo! Tô impressionade--:frowning_face:'))
+            sleep(2)
+        else:
+            print(emoji.emojize(':3rd_place_medal: Dá pra ver que multiplicação não é seu forte, mas não desista!'))
+    
+    repeticao = input('Quer jogar de novo?')
+    while repeticao[0].capitalize() != 'S' and repeticao[0].capitalize() != 'N':
+        repeticao = input(emoji.emojize('Não entendi :thinking_face:'))
+    if repeticao[0].capitalize() == 'N':
+        interface.play
+        print(emoji.emojize('Mas já? :sad_but_relieved_face:\nObrigade por jogar comigo! \nAté mais...:waving_hand:'))
+        sleep(3)
+        break
